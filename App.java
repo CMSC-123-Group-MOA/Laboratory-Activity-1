e47f66a0c3aca86dc1d4ee1c236dfb71e45d84ed
ali1x3 <avilaalex371@gmail.com>
2025-01-20 22:11:43 +0800

    no need for class folder, scrapped created the mainframe for the project

--- App.java
+++ App.java
@@ -1,5 +1,19 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
 public class App {
+    private static void init_Frame(JFrame frame){
+        BorderLayout borderLayout = new BorderLayout(); 
+        frame.setLayout(borderLayout);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(700, 500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args) {
-        System.out.print("hello");                
+        JFrame mainFrame = new JFrame();
+        init_Frame(mainFrame);
+        System.out.println("hello");                
     }
 }
