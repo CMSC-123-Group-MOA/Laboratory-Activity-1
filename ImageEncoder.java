103e51253de322e3addf2db3cbd8c31e2fa3dc9d
ddrhckrzz <andreimartillo2000@gmail.com>
2025-01-21 23:22:44 +0800

    made sample encoder work using relative path (please test)

--- ImageEncoder.java
+++ ImageEncoder.java
@@ -6,10 +6,10 @@ import javax.imageio.ImageIO;
 public class ImageEncoder {
 
     public static void main(String[] args) {
+        ImageEncoder ie = new ImageEncoder();
+        ClassLoader classLoader = ie.getClass().getClassLoader();
         try {
-
-            // Load the image
-            BufferedImage image = ImageIO.read(new File("C:\\Users\\AXIE\\Documents\\CMSC 123\\Laboratory 1\\Laboratory-Activity-1\\Images\\ap.PNG"));
+            BufferedImage image = ImageIO.read(classLoader.getResourceAsStream("Images/ap.PNG"));
 
             // Get image dimensions
             int width = image.getWidth();
@@ -17,12 +17,12 @@ public class ImageEncoder {
             int pixelData[][] = new int[width][height];
 
             // Loop through each pixel
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    // Get RGB value of the pixel
-                    pixelData[x][y] = image.getRGB(x, y);
-                }
-            }
+            // for (int y = 0; y < height; y++) {
+            //     for (int x = 0; x < width; x++) {
+            //         // Get RGB value of the pixel
+            //         pixelData[x][y] = image.getRGB(x, y);
+            //     }
+            // }
             System.out.println("Pixel Data Encoded!");
             // Print the encoded pixel data
             // for (int y = 0; y < height; y++) {
