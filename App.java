ba3d0b7bb9f50eb90f89d4157c2c6f8f765fab14
ddrhckrzz <andreimartillo2000@gmail.com>
2025-01-21 23:28:19 +0800

    removed redundant imports from App.java

--- App.java
+++ App.java
@@ -1,9 +1,3 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 import ui.MainFrame;
 
 public class App {
@@ -11,9 +5,5 @@ public class App {
     public static void main(String[] args) {
         MainFrame mainframe = new MainFrame();
         mainframe.start();
-<<<<<<< Updated upstream
-=======
-        
->>>>>>> Stashed changes
     }
 }
