a921b65c2f686d4de0a0c224ef6d86db3fc0ebdd
MoresVance <mcortega3@up.edu.ph>
2025-01-21 21:58:46 +0800

    finally added frequencyCounter class draft

---
+++ FrequencyCounter.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+public class FrequencyCounter {
+    HashMap<Integer, Integer> colorMap = new HashMap<Integer, Integer>();
+    int frequencyCount[];
+    int colorCount;
+    public FrequencyCounter(int[][] data, int width, int height){
+        colorCount = 0;
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y < height; y++){
+                if(colorMap.get(data[x][y]) == null){
+                    colorMap.put(data[x][y], colorCount);
+                    colorCount++;
+                }
+            }
+            System.out.println("hi");
+        }
+        System.out.println("done input");
+        System.out.println("Mapping of colors: " + colorMap);
+        frequencyCount = new int[colorMap.size()];
+
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y < height; y++){
+                for(int i : colorMap.keySet()){
+                    if(data[x][y] == colorMap.get(i)){
+                        frequencyCount[i]++;
+                    }
+                }
+                
+            }
+            System.out.println("hello");
+        }
+        System.out.println(" done1");
+        System.out.println("Number of Colors: " + colorMap.size());
+        for(int i = 0; i < frequencyCount.length; i++){
+            System.out.println("Color Key: " + i + " Frequency: " + frequencyCount[i]);
+        }
+    }    
+}
