9ca07dd7678ccb8b42bb7fd68ffe1778f2926b5e
ddrhckrzz <andreimartillo2000@gmail.com>
2025-01-21 16:31:40 +0800

    refactored the codebase
    added two new folders

--- App.java
+++ App.java
@@ -4,30 +4,15 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import ui.MainFrame;
+
 public class App {
     private static void init_Frame(JFrame frame){
-        BorderLayout borderLayout = new BorderLayout(); 
-        frame.setLayout(borderLayout);
-        frame.setLocationRelativeTo(null);
-        frame.setSize(700, 500);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
 
     public static void main(String[] args) {
-        // JFrame mainFrame = new JFrame();
-        // init_Frame(mainFrame);
-
-        // JPanel mainPanel = new JPanel();
-        // mainPanel.setBackground(Color.black);
-        // mainFrame.add(mainPanel, BorderLayout.CENTER);
-
-        // JPanel sidePanel = new JPanel();
-        // sidePanel.setBackground(Color.RED);
-        // sidePanel.setPreferredSize(new Dimension(300, 300));
+        MainFrame mainframe = new MainFrame();
         
-        // mainFrame.add(sidePanel, BorderLayout.EAST);
-             mainFrame mainframe = new mainFrame();
-                
     }
 }
--- ui/MainFrame.java
+++ ui/MainFrame.java
@@ -1,3 +1,5 @@
+package ui;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -5,21 +7,18 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
+import huff.AppVariables;
+
 import javax.swing.*;
 
-public class mainFrame extends JFrame {
+public class MainFrame extends JFrame {
     JPanel buttonsPanel = new JPanel();
     JButton add = new JButton();
     JButton compress = new JButton();
     JButton quit = new JButton();
 
-    public mainFrame(){
-        setTitle("Huffman Coding");
-        getContentPane().setPreferredSize(new Dimension(1000, 800));
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        setLayout(new BorderLayout());
+    public MainFrame() {
+        initLayout();
 
         buttonDesign(add, "ADD IMAGE");
         buttonDesign(compress, "COMPRESS");
@@ -30,21 +29,30 @@ public class mainFrame extends JFrame {
         buttonsPanel.setLayout(new GridBagLayout());
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.insets = new Insets(0,0,40,40);
+        gbc.insets = new Insets(0, 0, 40, 40);
         buttonsPanel.add(add, gbc);
-        gbc.gridx = 0; 
+        gbc.gridx = 0;
         gbc.gridy = 1;
-        gbc.insets = new Insets(0,0,40,40);
+        gbc.insets = new Insets(0, 0, 40, 40);
         buttonsPanel.add(compress, gbc);
-        gbc.gridx = 0; 
+        gbc.gridx = 0;
         gbc.gridy = 2;
-        gbc.insets = new Insets(0,0,40,40);
+        gbc.insets = new Insets(0, 0, 40, 40);
         buttonsPanel.add(quit, gbc);
 
         add(buttonsPanel, BorderLayout.EAST);
 
         setVisible(true);
         pack();
+    }
+
+    private void initLayout() {
+        setTitle(AppVariables.TITLE);
+        getContentPane().setPreferredSize(new Dimension(1000, 800));
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new BorderLayout());
     }
 
     public void buttonDesign(JButton button, String buttonName) {
